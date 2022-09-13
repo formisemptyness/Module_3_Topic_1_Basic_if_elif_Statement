e3a994b3c0b966f25a8771a921923b2ce3921e67
formisemptyness <jmmcginley@dmacc.edu>
2022-09-13 16:47:18 -0500

    Pseudocode has been written...

---
+++ basic_if_elif.py
@@ -0,0 +1,28 @@
+'''
+Program: basic_if_elif.py
+Author: Joshua M. McGinley
+Last date modified: 09/13/2022
+
+In the shell or in a text editor, write a program that asks for the user to sign up for Programmer's Toolkit Monthly
+Subscription Box. They must select level of membership they want. Each month is something different, t-shirts,
+stickers, figurines, even programming books!
+The levels are the following:
+
+    Platinum
+    Gold
+    Silver
+    Bronze
+    Free Trial
+
+Write an if statement that prints the cost for each of the level. Platinum is $60, each level below is 10 dollars
+cheaper, and the free trial is free.
+Submit your .py file.
+'''
+
+
+#Creat variables: Platinum ($60), Gold ($50), Silver ($40), Bronze ($30), and Free Trial (free) and set their value
+#Ask user to sing up for PTMSB (Programmer's Toolkit Monthly Subscription Box, ask which of the five levels
+    #want to sign up for
+#Take input from user and store in userSubLevel
+#Compare userSubLevel to the five variables using if statements
+    #Print the results: userSebLevel and its corasponding value
